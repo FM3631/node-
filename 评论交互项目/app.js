4c2fd9c3c02bbbc0645bf907771355dda85f3225
“伟” <1340597770@qq.com>
2020-04-10 17:07:13 +0800

    修改名字和评论

--- 评论交互项目/app.js
+++ 评论交互项目/app.js
@@ -14,7 +14,7 @@ var comments = [
     {name:'李四',message:'此评论不可见',dataTime:'2020/05/01'},
     {name:'王五',message:'此评论不可见',dataTime:'2020/05/01'},
     {name:'王五',message:'这个东西太棒了',dataTime:'2020/05/01'},
-    {name:'王五',message:'这个东西太棒了',dataTime:'2020/05/01'}
+    {name:'赵六',message:'这个东西太棒了',dataTime:'2020/05/01'}
 ]
 
 //创建服务
